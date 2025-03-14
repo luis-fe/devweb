a949b3664d83fb23b53d519050afeac8efcf6df3
luis-fe <luisengp@gmail.com>
2025-03-14 20:36:17 -0300

    com,mit

---
+++ src/connections/devConn.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+
+//Local Evironment - Ambiente local 
+export const DevDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port:5432,
+    username: "postgres",
+    password: "postgres",
+    database: "devweb",
+    //entides
+})
